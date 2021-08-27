a1a27fb44071bd96bc4272e1ee55363ae671a69a
unknown <orrcody7@gmail.com>
2021-08-27 12:15:28 -0400

    pushing all changes made to this project from the previous day

---
+++ superhero_project/superheroes/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Superhero
+
+# Create your views here.
+def index(request):
+    all_heroes = Superhero.objects.all()
+    context = {
+        'all_heroes': all_heroes
+    }
+    return render(request, 'superheroes/index.html', context)
+
+def detail(request, hero_id):
+    single_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'single_hero': single_hero
+    }
+    print(context)
+    return render(request, 'superheroes/detail.html', context)
+
+def create(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary = request.POST.get('primary')
+        secondary = request.POST.get('secondary')
+        catchphrase = request.POST.get('catchphrase')
+        new_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
+        new_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
+
+def update(request):
+    if request.method == "POST":
+        name = request.POST.update('name')
+        alter_ego = request.POST.update('alter_ego')
+        primary = request.POST.update('primary')
+        secondary = request.POST.update('seconday')
+        catchphrase = request.POST.update('catchphrase')
+        new_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
+        new_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/update.html')
+    
